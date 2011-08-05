d7022ad62df6f17d8cb52367fe41c17b5f06ef94
Jack Mitchell <jack@communistcode.co.uk>
2011-08-05 11:21:25 +0100

    added title varible to template

--- includes/template.inc.php
+++ includes/template.inc.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-		<title><?php echo("CCCMS : ".$pageContent['title']); ?></title>
+		<title><?php echo($title." : ".$pageContent['title']); ?></title>
 		<link href="stylesheet/stylesheet.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
