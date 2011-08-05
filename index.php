59cf917cc4f2d672f0ac888fc3e0e06455e12668
Jack Mitchell <jack@communistcode.co.uk>
2011-08-05 11:20:41 +0100

    added title option

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	require_once($fullPath."/classes/pageTools.class.php");
 	require_once($fullPath."/includes/global.inc.php");
 
+	$title = "CCCMS";
 	$content = $pageTools->matchTags($pageContent['text']);
 
 	require_once("includes/template.inc.php");
