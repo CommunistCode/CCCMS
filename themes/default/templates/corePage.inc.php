88b9129cc3527e7ed618cb41d0e3963e2207301c
Jack Mitchell <jack@communistcode.co.uk>
2011-09-02 15:12:11 +0100

    change encoding to utf-8

--- themes/default/templates/corePage.inc.php
+++ themes/default/templates/corePage.inc.php
@@ -2,7 +2,7 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title><?php echo($title." : ".$pageContent['title']); ?></title>
 		<link href="themes/default/stylesheets/base.css" rel="stylesheet" type="text/css" />
 	</head>
