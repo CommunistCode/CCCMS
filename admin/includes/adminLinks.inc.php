bfc0718e575b9b656429d7e9972bd450f718f7df
Jack Mitchell <jack@communistcode.co.uk>
2011-09-08 15:25:35 +0100

    make logout link appear last in admin area

--- admin/includes/adminLinks.inc.php
+++ admin/includes/adminLinks.inc.php
@@ -3,25 +3,35 @@
 	$mainLinks = $adminTools->getMainLinks();
 
 	while($mainRow = $mainLinks->fetch_assoc()) {
-	
-		echo("<div id='mainLink'><a class='mainLink' href='".$directoryPath."/".$mainRow['link']."'>".$mainRow['name']."</a></div>\n");
 
-		if (strcmp($adminTools->getCurrentCategory(),trim($mainRow['name'])) == 0) {
+		if (!strcmp($mainRow['name'],"Logout")) {
 
-			$subLinks = $adminTools->getSubLinks($mainRow['name']);
+			$logoutRow = $mainRow;
 
-			echo("<ul>");
+		} else {
 
-			while ($subRow = $subLinks->fetch_assoc()) {
+			echo("<div id='mainLink'><a class='mainLink' href='".$directoryPath."/".$mainRow['link']."'>".$mainRow['name']."</a></div>\n");
 
-				echo("<div id='subLink'><li><a class='subLink' href='".$directoryPath."/".$subRow['link']."'>".$subRow['name']."</a></li></div>\n");
+			if (strcmp($adminTools->getCurrentCategory(),trim($mainRow['name'])) == 0) {
 
-			}	
+				$subLinks = $adminTools->getSubLinks($mainRow['name']);
 
-			echo("</ul>");
+				echo("<ul>");
+
+				while ($subRow = $subLinks->fetch_assoc()) {
+
+					echo("<div id='subLink'><li><a class='subLink' href='".$directoryPath."/".$subRow['link']."'>".$subRow['name']."</a></li></div>\n");
+
+				}
+
+				echo("</ul>");
+
+			}
 
 		}
 
 	}
+	
+	echo("<div id='mainLink'><a class='mainLink' href='".$directoryPath."/".$logoutRow['link']."'>".$logoutRow['name']."</a></div>\n");
 
 ?>
