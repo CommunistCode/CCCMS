b82e4428ac37fedb7647aaa87c42440fd7ea4994
Jack Mitchell <jack@communistcode.co.uk>
2011-09-08 15:11:20 +0100

    add missing closing bracket from matchTags

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	require_once($fullPath."/classes/pageTools.class.php");
 	require_once($fullPath."/includes/global.inc.php");
 
-	$content = "<div id='matchTags'>".$pageTools->matchTags($pageContent['text']."</div>");
+	$content = "<div id='matchTags'>".$pageTools->matchTags($pageContent['text'])."</div>");
 
 	require_once($fullPath."/themes/".$pageTools->getTheme("base")."/templates/corePage.inc.php");
 
