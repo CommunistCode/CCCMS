881db2dea10273b070a8063ee401ba1e2af7bb89
Jack Mitchell <jack@communistcode.co.uk>
2012-01-22 11:16:38 +0000

    started modularising the admin area

---
+++ admin/includes/changePasswordForm.inc.php
@@ -0,0 +1,24 @@
+<p>Change your password for the administration area below</p>
+
+<form action='changePassword.php' method='post' name='changePass'>
+
+  <table>
+    <tr>
+      <td>Current Password</td>
+      <td><input type='password' name='currentPass' /></td>
+    </tr>
+    <tr>
+      <td>New Password</td>
+      <td><input type='password' name='newPass' /></td>
+    </tr>
+    <tr>
+      <td>Confirm Password</td>
+      <td><input type='password' name='confirmPass' /></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td><input type='submit' name='submit' value='Change Password' /></td>
+    </tr>
+  </table>
+
+</form>
