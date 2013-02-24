fc1343637b95c340bbd26bb8a7b3959669eb46fc
Jack Mitchell <jack@communistcode.co.uk>
2013-02-24 15:31:50 +0000

    strip out some uneeded error page info

--- error.php
+++ error.php
@@ -1,8 +1,5 @@
 <?php
 
-	require_once("config/config.php");
-	require_once($fullPath."/includes/global.inc.php");
-
   switch($_GET['type']) {
 
     case "database":
@@ -13,9 +10,5 @@
       $errorMessage = "An undefined error occurred, please try again";
 
   }
-
-  $content = $errorMessage;
-
-	require_once($fullPath."/themes/".$pageTools->getTheme("base")."/templates/corePage.inc.php");
 
 ?>
